4b24b5ecebc5fc448c003bb483b612487a5b27b7
andonov2000 <104697995+andonov2000@users.noreply.github.com>
2022-06-12 01:44:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ module.exports = (Plugin) =>
             const embed = new MessageEmbed()
               .setColor(guild.colour)
               .setTitle(`Вашият тикет е затворен.`)
-	      .addField("Тикет ID", "Благодарим Ви, че се свързахте с нас! Ако имате допълнително въпроси, създайте нов тикет.\n", true)
+	      .addField("\nБлагодарим Ви, че се свързахте с нас! Ако имате допълнително въпроси, създайте нов тикет.\n", "\n\n", true)
               .addField("Тикет ID", `\`${ticket.number}\` (#${channel_name})`, true)
               .addField("Отдел", `${category.name || "?"}`, true)
               .addField("Създаден от", `<@${ticket.creator}>`, true)
